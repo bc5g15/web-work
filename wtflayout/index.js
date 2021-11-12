a6391c7e05d9c804f8a4507539f4c2580e818441
Benedict Cairns <bcairns21@gmail.com>
2021-11-12 19:41:10 +0000

    Basic layout creation experiment

---
+++ wtflayout/index.js
@@ -0,0 +1,55 @@
+const topelem = document.getElementById('topcontainer')
+
+const defaultWidth = '10em'
+const defaultHeight = '2em'
+
+let currentElement = ''
+let blockCount = 0
+{
+    // Create controls 
+    const controlsContainer = document.getElementById('controls')
+    const addElemButton = document.createElement('button')
+    addElemButton.innerText = 'Create Element'
+    addElemButton.onclick = () => createElement(currentElement)
+    controlsContainer.appendChild(addElemButton)
+
+    const displayInput = document.createElement('input')
+    displayInput.type = 'text'
+    displayInput.size = '10'
+    controlsContainer.appendChild(displayInput)
+    
+    const setDisplayButton = document.createElement('button')
+    setDisplayButton.innerText = 'Set Display'
+    setDisplayButton.onclick = () => setDisplay(currentElement, displayInput.value)
+    controlsContainer.appendChild(setDisplayButton)
+}
+
+const createElement = (rootElemId) => {
+    if (!rootElemId) return
+    const elem = document.createElement('div')
+    elem.id = `block${blockCount++}`
+    elem.style.width = defaultWidth
+    elem.style.height = defaultHeight
+    elem.className = 'block'
+    elem.onclick = (ev) => selectElement(ev, elem.id)
+
+    document.getElementById(rootElemId).appendChild(elem)
+}
+
+const selectElement = (ev, elemId) => {
+    ev.stopPropagation()
+    if (currentElement) {
+        const oldElem = document.getElementById(currentElement)
+        oldElem.style.outline = ''
+    }
+    const elem = document.getElementById(elemId)
+    elem.style.outline = '1em solid purple'
+    currentElement = elemId;
+}
+
+const setDisplay = (elemId, displaySetting) => {
+    const elem = document.getElementById(elemId)
+    elem.style.display = displaySetting
+}
+
+topelem.onclick = (ev) => selectElement(ev, 'topcontainer')
