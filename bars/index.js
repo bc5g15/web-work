98063d141b7d014cdfae687b7bc0c033a87dca03
Benedict Cairns <bcairns21@gmail.com>
2021-11-27 11:22:06 +0000

    Add new bar and slider

--- bars/index.js
+++ bars/index.js
@@ -1,22 +1,33 @@
-const bar = document.getElementById('healthbar');
 let value = 100
-// Create buttons
+// Value Controls
 {
     const increaseButton = document.createElement('button')
     increaseButton.innerText = '+'
     increaseButton.onclick = () => {
         value++
-        bar.style.setProperty('--value', `${value}%`)
+        document.body.style.setProperty('--value', `${value}%`)
     }
     
     const decreaseBtn = document.createElement('button')
     decreaseBtn.innerText='-'
     decreaseBtn.onclick = () => {
         value--
-        bar.style.setProperty('--value', `${value}%`)
+        document.body.style.setProperty('--value', `${value}%`)
+    }
+
+    const slider = document.createElement('input')
+    slider.type = 'range'
+    slider.min = 0
+    slider.max = 100
+    slider.step = 1
+    slider.value=100
+    slider.oninput = () => {
+        value = slider.value
+        document.body.style.setProperty('--value', `${value}`)
     }
 
     document.body.appendChild(decreaseBtn)
     document.body.appendChild(increaseButton)
+    document.body.appendChild(slider)
 
 }
