a29f6cea215e2f7dba777c07882c94abae0db91f
Benedict Cairns <bcairns21@gmail.com>
2022-01-15 20:34:34 +0000

    Add palette and deletion

--- canvas/mappaint/index.js
+++ canvas/mappaint/index.js
@@ -1,4 +1,48 @@
+const stringify = (x, y) => `${x},${y}`
+const destringify = s => s.split(',').map(i => parseInt(i, 10))
+
 const canvas = document.getElementById('canvas')
+const palette = document.getElementById('palette')
+
+let tileValue = 1
+const colours = [
+    'null',
+    'white',
+    'red',
+    'blue'
+]
+
+
+// Populate palette, using default colours for now
+{
+    const boxes = []
+    for (const colour of colours) {
+        const colourBox = document.createElement('div')
+        colourBox.style.width = '5em'
+        colourBox.style.height = '5em'
+        colourBox.style.outline = colour === 'white' ? '1em solid white' : ''
+        if (colour === 'null') {
+            colourBox.innerText = 'X'
+            colourBox.style.lineHeight = '5em'
+            colourBox.style.textAlign = 'center'
+        } else {
+            colourBox.style.backgroundColor = colour
+        }
+
+        boxes.push(colourBox)
+
+        palette.append(colourBox)
+    }
+
+    for (const box in boxes) {
+        boxes[box].onclick = () => {
+            boxes.forEach(b => b.style.outline = '')
+            boxes[box].style.outline = '1em solid white'
+            tileValue = box
+            console.log(tileValue)
+        }
+    }
+}
 
 /** @type {CanvasRenderingContext2D} */
 const ctx = canvas.getContext('2d')
@@ -20,11 +64,6 @@ canvas.addEventListener('mousemove', (evt) => {
     mouseY = evt.clientY
 })
 
-const tiles = [
-    'black',
-    'red',
-    'blue'
-]
 
 const screenWidth = 640
 const screenHeight = 480
@@ -46,7 +85,7 @@ const map = [
 
 const drawMap = (map) => {
     map.forEach((arr, y) => arr.forEach((i, x) => {
-        ctx.fillStyle = tiles[i]
+        ctx.fillStyle = colours[i]
         ctx.fillRect(tileWidth * x, tileHeight * y, tileWidth, tileHeight)
     }))
 }
@@ -54,6 +93,7 @@ const drawMap = (map) => {
 drawMap(map)
 
 ctx.fillStyle = 'white'
+const paintedTiles = {}
 // Game Loop
 {
     const speed = 10
@@ -67,7 +107,16 @@ ctx.fillStyle = 'white'
         if (mouseClick) {
             const tileX = Math.floor(mouseX / tileWidth) * tileWidth
             const tileY = Math.floor(mouseY / tileHeight) * tileHeight
+
+            ctx.fillStyle = tileValue === '0' ? 'black' : colours[tileValue]
+            const positionString = stringify(tileX, tileY)
+            if (tileValue === '0' && positionString in paintedTiles) {
+                delete paintedTiles[positionString]
+            } else {
+                paintedTiles[positionString] = tileValue
+            }
             ctx.fillRect(tileX, tileY, tileWidth, tileHeight)
+
         }
         window.requestAnimationFrame(step)
     }
