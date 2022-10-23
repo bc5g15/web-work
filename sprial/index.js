12568967b1fd554699b6f9a70fa905715775b96a
Benedict Cairns <bcairns21@gmail.com>
2022-10-23 14:27:02 +0100

    Have adjustable thickness

--- sprial/index.js
+++ sprial/index.js
@@ -1,26 +1,26 @@
 const root = document.getElementById('spiral');
 
-// const v = 1;
-// const c = 0;
-// const w = 1;
-
-// const points = (t) => {
-//     const x = (v*t + c) * Math.cos(w * t);
-//     const y = (v*t + c) * Math.sin(w * t);
-//     return {x, y};
-// }
-
 /**
- * 
  * @param {string} type 
  * @returns {SVGAElement}
  */
 const makeSvgElem = (type) => document.createElementNS('http://www.w3.org/2000/svg', type)
 
+/** @param {Node} elem */
+const emptyNode = (elem) => {
+    while (elem.firstChild) {
+        elem.removeChild(elem.firstChild)
+    }
+}
+
 const lineElem = makeSvgElem('polyline');
 lineElem.style.stroke = 'white';
 lineElem.id = 'spiral-line'
 root.append(lineElem);
+
+const view = makeSvgElem('g');
+view.classList = 'rotater';
+root.append(view);
 
 const drawPoints = (v, c, w, maxPoints) => {
     const coords = (t) => {
@@ -38,12 +38,34 @@ const drawPoints = (v, c, w, maxPoints) => {
     lineElem.setAttribute('points', points);
 }
 
-// const polypoints = [];
-// for (let i = 0; i<100; i++) {
-//     polypoints.push(points(i));
-// }
-// const drawPath = polypoints.map(({x, y}) => `${x+100} ${y+100}`).join(' ');
-// lineElem.setAttribute('points', drawPath);
+const drawThickeningPoints = (v, c, w, maxPoints) => {
+    const coords = (t) => {
+        const x = (v*t + c) * Math.cos(w * t);
+        const y = (v*t + c) * Math.sin(w * t);
+        return {x, y};
+    }
+    emptyNode(view);
+
+    // const points = [];
+    let oldCoord = false;
+    for (let i = 1; i < maxPoints; i++) {
+        if (!oldCoord) {
+            oldCoord = coords(i);
+            continue;
+        }
+        const newCoord = coords(i);
+        const stepLine = makeSvgElem('line');
+        stepLine.setAttribute('x1', oldCoord.x + 100);
+        stepLine.setAttribute('y1', oldCoord.y + 100);
+        stepLine.setAttribute('x2', newCoord.x + 100);
+        stepLine.setAttribute('y2', newCoord.y + 100);
+        stepLine.style.stroke = 'white'
+        // stepLine.setAttribute('stroke', 'white');
+        stepLine.style.strokeWidth = ((i+1) * 0.05);
+        view.append(stepLine);
+        oldCoord = newCoord;
+    }
+}
 
 const submitButton = document.getElementById('submit');
 
@@ -52,5 +74,6 @@ submitButton.onclick = () => {
     const c = document.getElementById('c').valueAsNumber;
     const w = document.getElementById('w').valueAsNumber;
     // console.log(v);
-    drawPoints(v, c, w, 200);
+    // drawPoints(v, c, w, 200);
+    drawThickeningPoints(v, c, w, 200);
 }
