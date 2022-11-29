4da0b7e26cd7ed9d963fba1a1a64abf62822c284
Benedict Cairns <bcairns21@gmail.com>
2022-11-29 19:02:44 +0000

    Let user control values

--- wave/index.js
+++ wave/index.js
@@ -17,6 +17,42 @@ const drawPoint = (x, y) => {
 }
 
 for (let t = 0; t < 300; t++) {
-    let y = (Math.sin((Math.PI*2)*(t/300))*150) + 150;
+    let y = (Math.sin((Math.PI*2)*(t/300))*50) + 150;
     drawPoint(t, y)
+}
+
+// Create the controls and allow the wave to be configured
+
+const drawWave = (frequency, amplitude, offset) => {
+    for (let t = 0; t < 300; t++) {
+        let y = (Math.sin((Math.PI*2)*(t/300)*frequency + offset)*50*amplitude) + 150
+        drawPoint(t,y);
+    }
+}
+
+const controls = document.getElementById('controls')
+
+// Add the input controls
+{
+    const ampInput = document.createElement('input')
+    ampInput.type = 'number'
+    controls.append(ampInput);
+
+    const freqInput = document.createElement('input')
+    freqInput.type = 'number'
+    controls.append(freqInput)
+
+    const offsetInput = document.createElement('input')
+    offsetInput.type = 'number'
+    controls.append(offsetInput)
+
+    const submitButton = document.createElement('button')
+    submitButton.innerText = 'Draw Wave'
+    submitButton.onclick = () => {
+        const amp = ampInput.valueAsNumber
+        const freq = freqInput.valueAsNumber
+        const offset = offsetInput.valueAsNumber
+        drawWave(freq, amp, offset)
+    }
+    controls.append(submitButton)
 }
